33818ac77ca76c4a15e9c1f8ab4a2ab7511d8f86
syxxzzr <syxxzzr@163.com>
2025-05-24 22:35:07 +0800

    feat: add breadcrumb to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
+import { Breadcrumb, BreadcrumbList, BreadcrumbPage } from '@/components/ui/breadcrumb'
 import SidebarTrigger from '@/components/SidebarTrigger.vue'
 import ThemeSwitcher from '@/components/ThemeSwitcher.vue'
 </script>
 
 <template>
   <header
-    class="flex h-12 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-9"
+    class="flex h-12 shrink-0 items-center transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-9"
   >
     <SidebarTrigger class="m-2" />
+    <Breadcrumb>
+      <BreadcrumbList class="text-base">
+        <span class="text-[var(--color-muted-foreground)] select-none">|</span>
+        <BreadcrumbPage>
+          {{ $route.name }}
+        </BreadcrumbPage>
+      </BreadcrumbList>
+    </Breadcrumb>
+    <div class="text-[var(--color-muted-foreground)]"></div>
     <ThemeSwitcher class="ml-auto mr-2" />
   </header>
 </template>
