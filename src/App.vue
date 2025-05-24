cacac577611839933c953b87f4a8976ccafbe60c
syxxzzr <syxxzzr@163.com>
2025-05-24 20:38:26 +0800

    fix: wrong component spirit name

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { SidebarProvider } from '@/components/ui/sidebar'
-import SideBar from '@/components/SideBar.vue'
+import Sidebar from '@/components/Sidebar.vue'
 import SidebarTrigger from '@/components/SidebarTrigger.vue'
 </script>
 
 <template>
   <main>
     <SidebarProvider>
-      <SideBar />
+      <Sidebar />
       <SidebarTrigger />
     </SidebarProvider>
   </main>
