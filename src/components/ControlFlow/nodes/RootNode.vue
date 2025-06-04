5352a9b1bed18ae72ed81fc33ee1b03d13e06b08
syxxzzr <syxxzzr@163.com>
2025-06-04 22:02:25 +0800

    feat: update the style of root node demo

--- src/components/ControlFlow/nodes/RootNode.vue
+++ src/components/ControlFlow/nodes/RootNode.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
 import { Handle, Position } from '@vue-flow/core'
-import NodeBrock from '@/components/ControlFlow/nodes/NodeBrock.vue'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { CirclePlay } from 'lucide-vue-next'
-
-interface Props {
-  id: string
-  data: {
-    format: VideoFormat
-  }
-}
-const props = defineProps<Props>()
-function onConnect() {
-  props.data.format
-}
+import { Card, CardContent } from '@/components/ui/card'
+import { Film } from 'lucide-vue-next'
 </script>
 
 <template>
-  <Card class="w-70 h-34 gap-4">
-    <CardHeader>
-      <CardTitle>core</CardTitle>
-    </CardHeader>
+  <Card>
     <CardContent class="flex flex-row items-center space-x-4">
-      <CirclePlay :size="45" color="#41B883" class="bg-green-500/10 rounded-sm p-1" />
-      <p class="text-xl">1 Tasks Running</p>
+      <Film :size="45" class="bg-accent opacity-80 rounded-sm p-1" />
+      <div class="flex flex-col space-y-1.5">
+        <p class="text-xl">Source Video</p>
+        <div class="text-sm opacity-80">
+          <p>YUV420P8 30fps</p>
+          <p>01:22:31</p>
+        </div>
+      </div>
     </CardContent>
   </Card>
-  <Handle :position="Position.Right" />
+  <Handle :position="Position.Right" type="source" />
 </template>
